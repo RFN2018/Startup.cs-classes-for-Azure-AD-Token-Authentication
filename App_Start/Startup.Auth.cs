dfcbc5af7e4ef18186e139369210c45e5063f41e
RFN2018 <44893745+RFN2018@users.noreply.github.com>
2019-01-22 11:17:46 +0000

    added files via upload

---
+++ App_Start/Startup.Auth.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.ActiveDirectory;
+using Owin;
+
+namespace WebApplication6
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseWindowsAzureActiveDirectoryBearerAuthentication(
+                new WindowsAzureActiveDirectoryBearerAuthenticationOptions
+                {
+                    Tenant = ConfigurationManager.AppSettings["ida:Tenant"],
+                    TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidAudience = ConfigurationManager.AppSettings["ida:Audience"],
+                    },
+                });
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,15 @@
+﻿using Owin;
+
+
+
+
+namespace WebApplication6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
